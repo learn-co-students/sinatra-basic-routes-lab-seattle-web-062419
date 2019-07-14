90ff4981f1ef798f83c05b789a5866822d6ecf88
aminamos <26092352+aminamos@users.noreply.github.com>
2019-07-13 22:32:43 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Your options are:\n
+    -/name\n
+    -/hometown\n
+    -/favorit-song"
+  end
+  
+  get '/name' do
+    "My name is __"
+  end
+
+  get '/hometown' do
+    "My hometown is __"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is __"
+  end
 end
